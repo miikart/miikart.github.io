94d3e33437034480afe4e5f8ab3b1d33caf82158
miikart <92797301+miikart@users.noreply.github.com>
2024-01-18 14:01:50 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,27 @@
 # ai gened btw
-# will remove later
 #!/bin/bash
 
-while true; do
-  clear
-  # Delay of approximately 1 millisecond (not guaranteed to be precise)
+options=("Option 1" "Option 2" "Option 3" "Exit")
+
+PS3="Please select an option: "  # Set prompt for select
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "You chose Option 1."
+            ;;
+        "Option 2")
+            echo "You chose Option 2."
+            ;;
+        "Option 3")
+            echo "You chose Option 3."
+            ;;
+        "Exit")
+            break
+            ;;
+        *)
+            echo "Invalid option."
+            ;;
+    esac
 done
