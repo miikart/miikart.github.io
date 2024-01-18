8001766894453f64118bca872ae406acf6d4c146
miikart <92797301+miikart@users.noreply.github.com>
2024-01-18 14:08:00 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,27 +1,23 @@
-# ai gened btw
 #!/bin/bash
 
-options=("Option 1" "Option 2" "Option 3" "Exit")
-
-PS3="Please select an option: "  # Set prompt for select
-
+PS3='Please enter your choice: '
+options=("Option 1 (Exit Bash)" "Option 2" "Option 3" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
-            echo "You chose Option 1."
+        "Option 1 (Exit Bash)")
+            exit  # This will exit the Bash shell
             ;;
         "Option 2")
-            echo "You chose Option 2."
+            echo "You chose Option 2!"
             ;;
         "Option 3")
-            echo "You chose Option 3."
+            echo "You chose Option 3!"
             ;;
-        "Exit")
-            break
+        "Quit")
+            break  # This will exit the select loop
             ;;
-        *)
-            echo "Invalid option."
+        *) echo "Invalid option. Please try again."
             ;;
     esac
 done
